2368efbe58bd7c80ccebff4e7aa718c2558e88e0
abemba <42939449+abemba@users.noreply.github.com>
2023-12-15 00:26:53 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    STRIPE_PUBLIC_KEY: "pk_live_B4uBuK4S72gPaLmTjd69EEHr",
-    apiEndpoint: "app.algofame.com/api/"
+    STRIPE_PUBLIC_KEY: "",
+    apiEndpoint: ""
 };
